f83445e9d68d6ece01d9595ffd72a9878fe82e71
yanzijie <yanzijie@foresee.com.cn>
2021-05-14 21:11:28 +0800

    测试提交到分支

--- ruoyi-common/src/main/java/com/ruoyi/common/utils/uuid/IdUtils.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/utils/uuid/IdUtils.java
@@ -4,14 +4,14 @@ import com.ruoyi.common.utils.uuid.UUID;
 
 /**
  * ID生成器工具类
- * 
- * @author ruoyi
+ *
+ * @author ruoyi 测试
  */
 public class IdUtils
 {
     /**
      * 获取随机UUID
-     * 
+     *
      * @return 随机UUID
      */
     public static String randomUUID()
@@ -21,7 +21,7 @@ public class IdUtils
 
     /**
      * 简化的UUID，去掉了横线
-     * 
+     *
      * @return 简化的UUID，去掉了横线
      */
     public static String simpleUUID()
@@ -31,7 +31,7 @@ public class IdUtils
 
     /**
      * 获取随机UUID，使用性能更好的ThreadLocalRandom生成UUID
-     * 
+     *
      * @return 随机UUID
      */
     public static String fastUUID()
@@ -41,7 +41,7 @@ public class IdUtils
 
     /**
      * 简化的UUID，去掉了横线，使用性能更好的ThreadLocalRandom生成UUID
-     * 
+     *
      * @return 简化的UUID，去掉了横线
      */
     public static String fastSimpleUUID()
