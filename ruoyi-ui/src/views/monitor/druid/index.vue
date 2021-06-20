4599c512f9b2a31654f83ac0e78dad5cc1004030
thh <18776050775@163.com>
2021-06-20 08:12:20 +0800

    【add】封装iframe组件

--- ruoyi-ui/src/views/monitor/druid/index.vue
+++ ruoyi-ui/src/views/monitor/druid/index.vue
@@ -1,26 +1,15 @@
 <template>
-  <div v-loading="loading" :style="'height:'+ height">
-    <iframe :src="src" frameborder="no" style="width: 100%;height: 100%" scrolling="auto" />
-  </div>
+  <i-frame :src="url" />
 </template>
 <script>
-export default {
-  name: "Druid",
-  data() {
-    return {
-      src: process.env.VUE_APP_BASE_API + "/druid/login.html",
-      height: document.documentElement.clientHeight - 94.5 + "px;",
-      loading: true
-    };
-  },
-  mounted: function() {
-    setTimeout(() => {
-      this.loading = false;
-    }, 230);
-    const that = this;
-    window.onresize = function temp() {
-      that.height = document.documentElement.clientHeight - 94.5 + "px;";
-    };
-  }
-};
+  import iFrame from "@/components/iFrame/index";
+  export default {
+    name: "Druid",
+    components: { iFrame },
+    data() {
+      return {
+        url: process.env.VUE_APP_BASE_API + "/druid/login.html"
+      };
+    },
+  };
 </script>
--- ruoyi-ui/src/views/tool/swagger/index.vue
+++ ruoyi-ui/src/views/tool/swagger/index.vue
@@ -1,26 +1,15 @@
 <template>
-  <div v-loading="loading" :style="'height:'+ height">
-    <iframe :src="src" frameborder="no" style="width: 100%;height: 100%" scrolling="auto" />
-  </div>
+  <i-frame :src="url" />
 </template>
 <script>
-export default {
-  name: "Swagger",
-  data() {
-    return {
-      src: process.env.VUE_APP_BASE_API + "/swagger-ui/index.html",
-      height: document.documentElement.clientHeight - 94.5 + "px;",
-      loading: true
-    };
-  },
-  mounted: function() {
-    setTimeout(() => {
-      this.loading = false;
-    }, 230);
-    const that = this;
-    window.onresize = function temp() {
-      that.height = document.documentElement.clientHeight - 94.5 + "px;";
-    };
-  }
-};
+  import iFrame from "@/components/iFrame/index";
+  export default {
+    name: "Swagger",
+    components: { iFrame },
+    data() {
+      return {
+        url: process.env.VUE_APP_BASE_API + "/swagger-ui/index.html"
+      };
+    },
+  };
 </script>
