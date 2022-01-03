720057c828c927600be5a564b1616a664917705a
thh <18776050775@163.com>
2022-01-03 11:26:45 +0800

    【fix】删除nuxt-web

--- web-ui/nuxt-web/api/website/webConfig.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-
-// 查询站点配置详细
-export function getWebConfig() {
-  return request({
-    url: '/website/webConfig/getWebConfig' ,
-    method: 'post'
-  })
-}
--- web-ui/nuxt-web/config/config.js
+++
@@ -1,3 +0,0 @@
-export const fileUploadHost = 'http://127.0.0.1:8600'
-export const baseURL = 'http://127.0.0.1:7070'
-
