5413539ccc8638bf73d1b1a97a1990d678eb5349
tanhuihuang <tanhuihuang@richinfo.cn>
2022-05-11 09:58:55 +0800

    【fix】页面若未匹配到字典标签则返回原字典值

--- web-ui/vue-admin/src/utils/ruoyi.js
+++ web-ui/vue-admin/src/utils/ruoyi.js
@@ -70,6 +70,9 @@ export function addDateRange(params, dateRange, propName) {
 
 // 回显数据字典
 export function selectDictLabel(datas, value) {
+  if (value === undefined) {
+    return '';
+  }
   var actions = [];
   Object.keys(datas).some((key) => {
     if (datas[key].value == ('' + value)) {
@@ -82,18 +85,23 @@ export function selectDictLabel(datas, value) {
 
 // 回显数据字典（字符串数组）
 export function selectDictLabels(datas, value, separator) {
-  if(value === undefined) {
+  if (value === undefined) {
     return "";
   }
   var actions = [];
   var currentSeparator = undefined === separator ? "," : separator;
   var temp = value.split(currentSeparator);
   Object.keys(value.split(currentSeparator)).some((val) => {
+    var match = false;
     Object.keys(datas).some((key) => {
       if (datas[key].value == ('' + temp[val])) {
         actions.push(datas[key].label + currentSeparator);
+        match = true;
       }
     })
+    if (!match) {
+      actions.push(temp[val] + currentSeparator);
+    }
   })
   return actions.join('').substring(0, actions.join('').length - 1);
 }
