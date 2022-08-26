19d497e131a0d8ef674ba9b9f45bbbd451318e1a
tanhuihuang <tanhuihuang@richinfo.cn>
2022-08-26 14:21:04 +0800

    【fix】防止用户个人信息修改部门

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysProfileController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysProfileController.java
@@ -68,6 +68,7 @@ public class SysProfileController extends BaseController {
         user.setUserId(sysUser.getUserId());
         user.setPassword(null);
         user.setAvatar(null);
+        user.setDeptId(null);
         if (userService.updateUserProfile(user) > 0) {
             // 更新缓存用户信息
             sysUser.setNickName(user.getNickName());
