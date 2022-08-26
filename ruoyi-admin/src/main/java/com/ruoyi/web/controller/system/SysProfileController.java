8a00be2582e60152c31ada77f5fb1549219c5bb0
tanhuihuang <tanhuihuang@richinfo.cn>
2022-08-26 14:20:24 +0800

    【fix】优化修改资料头像被覆盖的问题

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysProfileController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysProfileController.java
@@ -67,6 +67,7 @@ public class SysProfileController extends BaseController {
         }
         user.setUserId(sysUser.getUserId());
         user.setPassword(null);
+        user.setAvatar(null);
         if (userService.updateUserProfile(user) > 0) {
             // 更新缓存用户信息
             sysUser.setNickName(user.getNickName());
