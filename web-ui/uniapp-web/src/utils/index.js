ba4567a36be7abb695e8e4017ecd81026aab9e42
tanhuihuang <tanhuihuang@richinfo.cn>
2022-11-20 13:36:12 +0800

    【fix】优化字典回显方法

--- web-ui/uniapp-web/src/utils/index.js
+++ web-ui/uniapp-web/src/utils/index.js
@@ -149,8 +149,14 @@ export function selectDictLabel(datas, value) {
 	return actions.join('');
 }
 
-// 回显数据字典（字符串数组）
+// 回显数据字典（字符串、数组）
 export function selectDictLabels(datas, value, separator) {
+	if (value === undefined || value.length ===0) {
+		return "";
+	}
+	if (Array.isArray(value)) {
+		value = value.join(",");
+	}
 	let actions = [];
 	const currentSeparator = undefined === separator ? "," : separator;
 	const temp = value.split(currentSeparator);
