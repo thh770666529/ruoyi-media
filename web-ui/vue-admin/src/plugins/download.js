e10be27e3fda29b66f3b6c44ba896a5b07047f5d
tanhuihuang <tanhuihuang@richinfo.cn>
2022-12-12 15:43:45 +0800

    【fix】encodeURI是对url中的查询字符串部分进行转义 encodeURIComponent对整个url进行转义，包括空格、英文冒号、斜杠等

--- web-ui/vue-admin/src/plugins/download.js
+++ web-ui/vue-admin/src/plugins/download.js
@@ -9,7 +9,7 @@ const baseURL = process.env.VUE_APP_BASE_API
 
 export default {
   name(name, isDelete = true) {
-    var url = baseURL + "/common/download?fileName=" + encodeURI(name) + "&delete=" + isDelete
+    var url = baseURL + "/common/download?fileName=" + encodeURIComponent(name) + "&delete=" + isDelete
     axios({
       method: 'get',
       url: url,
@@ -26,7 +26,7 @@ export default {
     })
   },
   resource(resource) {
-    var url = baseURL + "/common/download/resource?resource=" + encodeURI(resource);
+    var url = baseURL + "/common/download/resource?resource=" + encodeURIComponent(resource);
     axios({
       method: 'get',
       url: url,
