b68f3c38ddd0fe54b647f153e37156e9182a0694
tanhuihuang <tanhuihuang@richinfo.cn>
2022-12-12 15:54:25 +0800

    【fix】定时任务违规的字符

--- ruoyi-common/src/main/java/com/ruoyi/common/constant/Constants.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/constant/Constants.java
@@ -137,7 +137,7 @@ public class Constants {
      * 定时任务违规的字符
      */
     public static final String[] JOB_ERROR_STR = {"java.net.URL", "javax.naming.InitialContext", "org.yaml.snakeyaml",
-            "org.springframework", "org.apache", "com.ruoyi.common.utils.file"};
+            "org.springframework", "org.apache", "com.ruoyi.common.utils.file", "com.ruoyi.common.config" };
 
     /**
      * 通用开关(开启)
